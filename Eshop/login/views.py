f0729e5fd11dc488a22ee0d9b0cdb24f01423482
unknown <870103948@qq.com>
2019-02-19 19:32:54 +0800

    yanzheng

--- Eshop/login/views.py
+++ Eshop/login/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render,redirect
 
+from login.models import User # 要不然下面不行
 # Create your views here.
 def index(request):
     pass
     return render(request,'login/index.html')
 
 def login(request):
-    pass
-    return render(request,'login/login.html')
+    if request.method == "POST":
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        message = "It can not be empty"
+        if username and password:  # 确保用户名和密码都不为空
+            username = username.strip()
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = User.objects.get(name=username)
+
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "password is wrong"
+            except:
+                message = "User is not exist"
+        return render(request, 'login/login.html',{"message":message})
+    return render(request, 'login/login.html')
+
 def register(request):
     pass
     return render(request,'login/register.html')
