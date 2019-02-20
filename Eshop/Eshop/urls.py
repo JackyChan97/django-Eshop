1d437bab63eca1e2389c7fa79336910100e7c725
unknown <870103948@qq.com>
2019-02-20 21:35:37 +0800

    log

--- Eshop/Eshop/urls.py
+++ Eshop/Eshop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from login import views
+from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/', views.index),
     url(r'^login/', views.login),
     url(r'^register/', views.register),
     url(r'^logout/', views.logout),
+    url(r'^captcha', include('captcha.urls')) ,
 ]
--- Eshop/login/forms.py
+++ Eshop/login/forms.py
@@ -1,7 +1,18 @@
 
 from django import forms
 
+from captcha.fields import CaptchaField
 
 class UserForm(forms.Form):
     username = forms.CharField(label="UserName", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="Password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    captcha = CaptchaField(label='Captcha')
+
+class RegisterForm(forms.Form):
+
+    username = forms.CharField(label="UserName", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Paswword again", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    captcha = CaptchaField(label='Captcha')
